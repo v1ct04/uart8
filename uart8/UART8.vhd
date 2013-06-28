91c9c6499abb3db18e319bfcb09ee4a9c37174da
Victor Elias <victorgelias@gmail.com>
2013-06-28 05:07:38 -0300

    Creating dummy files for each module

---
+++ uart8/UART8.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    05:03:27 06/28/2013 
+-- Design Name: 
+-- Module Name:    UART8 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity UART8 is
+end UART8;
+
+architecture Behavioral of UART8 is
+
+begin
+
+
+end Behavioral;
+
