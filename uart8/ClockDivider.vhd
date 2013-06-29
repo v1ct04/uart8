c6504f33f1ead722afae3b5326d00956a6d9a2da
Victor Elias <victorgelias@gmail.com>
2013-06-29 06:45:33 -0300

    Removing auto generated header comments from files

--- uart8/ClockDivider.vhd
+++ uart8/ClockDivider.vhd
@@ -1,33 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: Instituto Tecnológico de Aeronáutica
--- Engineer: Victor Elias
--- 
--- Create Date:    05:05:11 06/28/2013 
--- Design Name: 
--- Module Name:    ClockDivider - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ClockDivider is
 	generic(
--- uart8/CryptoModule.vhd
+++ uart8/CryptoModule.vhd
@@ -1,31 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    05:07:06 06/28/2013 
--- Design Name: 
--- Module Name:    CryptoModule - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity CryptoModule is
 	PORT(
